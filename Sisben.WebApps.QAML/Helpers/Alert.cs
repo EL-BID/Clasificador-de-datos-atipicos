5d8f9cd8218b8c2234ef81b643f75110cac5cfe1
Carlos Tejada <carlostejada@gmail.com>
2018-03-09 19:36:18 +0000

    First commit
    First stable version. This version implements the following:
    * View DEPARTAMENTOS, how many have been processed and how many are
      irregular
    * View MUNICIPIOS of a DEPARTAMENTO, how many have been processed
      and how many are irregular
    * View the details of a HOME in a MUNICIPIO and the classification
      that the algorithm gave to each variable

---
+++ Sisben.WebApps.QAML/Helpers/Alert.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sisben.WebApps.QAML.Helpers
+{
+    public static class Alert
+    {
+        public static string GetClass(double score)
+        {
+            string result = string.Empty;
+            if (score >= 0.75)
+                result = "alert-success";
+            else if (score >= 0.25)
+                result = "alert-warning";
+            else
+                result = "alert-danger";
+            return result;
+        }
+    }
+}
