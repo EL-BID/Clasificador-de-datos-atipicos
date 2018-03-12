98181fc7e6431b16f0f64d2a59d2094fd7992c87
Carlos Tejada <carlostejada@gmail.com>
2018-03-12 13:11:42 -0400

    Added XML documentation to the C# code

--- src/Sisben.WebApps.QAML/Helpers/Alert.cs
+++ src/Sisben.WebApps.QAML/Helpers/Alert.cs
@@ -5,8 +5,22 @@ using System.Web;
 
 namespace Sisben.WebApps.QAML.Helpers
 {
+    /// <summary>
+    /// La clase Alert es una clase "helper" estática con un método para determinar el tipo de alerta
+    /// que amerita la puntuación de una variable de un hogar.
+    /// </summary>
     public static class Alert
     {
+        /// <summary>
+        /// Obtiene la clase de alerta de bootstrap correspondiente para la puntuación de la variable.
+        /// </summary>
+        /// <param name="score">Puntuación de nivel de confianza que obtuvo la variable.</param>
+        /// <returns>
+        /// Retorna los siguientes resultados dependiendo de la puntuación (score):
+        /// * "alert-danger" para el intervalo [0, 0.25)
+        /// * "alert-warning" para el intervalo [0.25, 0.75)
+        /// * "alert-success" para el intervalo [0.75, 1]
+        /// </returns>
         public static string GetClass(double score)
         {
             string result = string.Empty;
